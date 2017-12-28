1d1aaf60d3e3f08fadafa20e2516ec737343ef35
aliandra <aphelan10@gmail.com>
2017-12-28 03:28:52 -0500

    Easy #343: Major Scales

---
+++ easy343_MajorScales.py
@@ -0,0 +1,41 @@
+def getMajorScale(major):
+    """
+    Return major scale.
+
+    The notes in a major scale are the notes that are 0, 2, 4, 5, 7, 9,
+    and 11 semitones above the note that the scale is named after.
+    """
+    chromatic_scale = [
+        'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
+    ]
+    major_index = chromatic_scale.index(major)
+    chromatic_scale = (chromatic_scale[major_index:] +
+                       chromatic_scale[:major_index])
+    index = [0, 2, 4, 5, 7, 9, 11]
+    major_scale = []
+    for i in index:
+        major_scale.append(chromatic_scale[i])
+    return major_scale
+
+
+def note(major, solfege):
+    """
+    Return the solfège note of the major scale.
+    """
+    major_scale = getMajorScale(major)
+    note_name = {
+        "Do": 0, "Re": 1, "Mi": 2, "Fa": 3, "So": 4, "La": 5, "Ti": 6
+    }
+    return major_scale[note_name[solfege]]
+
+
+def test():
+    assert note("C", "Do") == "C"
+    assert note("C", "Re") == "D"
+    assert note("C", "Mi") == "E"
+    assert note("D", "Mi") == "F#"
+    assert note("A#", "Fa") == "D#"
+    print("All tests passed")
+
+
+test()
